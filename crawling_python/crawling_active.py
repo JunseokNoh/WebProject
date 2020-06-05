c2ff37f3b01980f8a3ba6edf4e8bbf2dc7c531a0
hackery00bi <hackery00bi@gmail.com>
2020-06-05 17:09:53 +0900

    modified crawling_action

--- crawling_python/crawling_active.py
+++ crawling_python/crawling_active.py
@@ -2,7 +2,13 @@ import crawling_naver_move_rank
 
 
 def main():
-    naverMovieRank = crawling_naver_move_rank.NaverMovieCrawling()
+    naverMovieRank = crawling_naver_move_rank.NaverMovieCrawling('https://movie.naver.com/movie/sdb/rank/rmovie.nhn',
+                                                                 'https://movie.naver.com/',
+                                                                 'localhost',
+                                                                 'yoobi',
+                                                                 'toor',
+                                                                 'jsp_db',
+                                                                 'utf8')
 
     naverMovieRank.crawler()
 
