33c6ef0031f5fdef93f4d81a460102c76c80a7b0
JSeoYoung <tsi04099@gmail.com>
2020-06-06 19:18:51 +0900

    add crawling_yes24_book_rank.py

---
+++ crawling_python/crawling_yes24_book_rank.py
@@ -0,0 +1,60 @@
+import pymysql
+import requests
+from bs4 import BeautifulSoup
+from abc import *
+
+import crawling
+
+
+class KyoboBookCrawling(crawling.Crawling, ABC):
+    def __init__(self, main_url, article_url, db_host, db_user, db_pw, db_name, db_charset):
+        super().__init__(main_url, article_url, db_host, db_user, db_pw, db_name, db_charset)
+
+    def crawler(self):
+        try:
+            url = super().MAIN_URL()
+            req = requests.get(url)
+            cont = req.content
+            soup = BeautifulSoup(cont, 'lxml')
+
+            # print(soup)
+            soup = soup.select("div#bestList > ol > li")
+            # print(soup)
+
+            for i in range(len(soup)):
+                soup[i] = soup[i].select("p")
+
+                BOOK_TITLE = soup[i][2].find("a").get_text()
+                BOOK_URL = "http://www.yes24.com" + soup[i][2].find("a")["href"]
+
+                temp = soup[i][3].get_text().split("|");
+                BOOK_AUTHOR = temp[0][0:len(temp[0]) - 3]
+                BOOK_PUBLISHER = temp[1][1:]
+
+                self.connect_db(i, BOOK_TITLE, BOOK_URL, BOOK_AUTHOR, BOOK_PUBLISHER)
+                #print(str(i + 1) + " : " + BOOK_TITLE + " : " + BOOK_URL + " : " + BOOK_AUTHOR + " : " + BOOK_PUBLISHER)
+
+        except Exception as e:
+            super().error_logging(str(e))
+            print("Error Detected")
+
+    def connect_db(self, i, book_title, book_info_url, book_author, book_publisher):
+        rank_number = i + 1
+        conn = pymysql.connect(host=super().DB_HOST(),
+                               user=super().DB_USER(),
+                               password=super().DB_PW(),
+                               db=super().DB_NAME(),
+                               charset=super().DB_CHARSET())
+        curs = conn.cursor()
+
+        sql = """select title from yes24_book_rank where rank = %s"""
+        curs.execute(sql, rank_number)
+        row = curs.fetchone()
+        if row[0] == book_title:
+            print("same yes24")
+        else:
+            sql = """update yes24_book_rank set title=%s, url=%s, author=%s, publisher=%s where rank=%s"""
+            curs.execute(sql, (book_title, book_info_url, book_author, book_publisher, rank_number))
+
+        conn.commit()
+        conn.close()
